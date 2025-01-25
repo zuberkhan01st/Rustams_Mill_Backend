1221288f24256fd5b105ce0246efcfcd7c6ba835
Zuber Khan <zuberkhan01st@gmail.com>
2025-01-25 13:14:22 +0530

    Updated Grievance Feature

--- models/grievance.js
+++ models/grievance.js
@@ -4,6 +4,8 @@ const grievanceSchema = new mongoose.Schema({
     imageURL: { type: String, required: true ,unique:true},
     description: { type: String, required: true },
     title :{ type: String, required:true},
+    name:{ type: String,required:true},
+    no:{type: Number, required:true},
     createdAt: {
         type: Date,
         default: Date.now
